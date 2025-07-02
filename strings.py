c00463cbe29fb7fa1c76d359cf2530e6a4dda797
ajay-973 <guntreddiajay234@gmail.com>
2025-07-02 21:33:06 +0530

    ms
    skjxn

---
+++ strings.py
@@ -0,0 +1,57 @@
+#strings
+
+#strings are immmutable data type
+
+#A string is a sequence of characters
+  
+#single = 'hello world!'
+#print(single)
+#double="hello world!"
+#print(double)
+#triple='''Hello world!'''
+#print(triple)
+
+
+#sample = """ welcome to python life "he said,join by 8 o'clock" """
+#print(sample)
+#print(type(sample))
+
+#sample=' '
+#print(type(sample))
+
+#sample_2=str()
+#print(type(sample_2))
+
+#sentence="pythonlife"
+#print(sentence[-1::-1]
+
+
+#text = "python programming"
+#capitalized_text=text.capitalize()
+#print(capitalized_text)
+
+################ Task ############
+
+
+#sentence ="python is amazing"
+#i=0
+#char=[]
+#for i in range(0,len(sentence),2):
+#    char.append(sentence[i])
+#print(char)
+
+
+#character="welcome to my world"
+#print(character.replace(" ","_"))
+
+
+#numeric_string="12345"
+#alpha_string="python"
+#is_char=alpha_string.isalpha()
+#print(is_char)
+
+
+#s="python is amazing"
+#print(s[::-1])
+
+
